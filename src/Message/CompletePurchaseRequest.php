f556c5781554ba0b435d643ddb66a0f9829ecce8
razmik <razmik.saghoyan@betconstruct.com>
2018-12-25 19:13:41 +0400

    changed complate purchase logic

--- src/Message/CompletePurchaseRequest.php
+++ src/Message/CompletePurchaseRequest.php
@@ -2,14 +2,70 @@
 
 namespace Omnipay\YandexMoney\Message;
 
-use Omnipay\YandexMoney\Helpers\ParametersTrait;
-
 /**
  * YandexMoney Complete Purchase Request
  */
 class CompletePurchaseRequest extends AbstractRequest
 {
-    use ParametersTrait;
+
+    /**
+     * Sets the request Amount
+     * @param $value
+     * @return mixed
+     */
+    public function setAmount($value)
+    {
+        return $this->setParameter('amount', $value);
+    }
+
+    /**
+     * Get Amount
+     * @return mixed
+     */
+    public function getAmount()
+    {
+        return $this->getParameter('amount');
+    }
+
+    /**
+     * Sets the request Currency
+     * @param $value
+     * @return mixed
+     */
+    public function setCurrency($value)
+    {
+        return $this->setParameter('currency', $value);
+    }
+
+    /**
+     * Get Currency
+     * @return mixed
+     */
+    public function getCurrency()
+    {
+        return $this->getParameter('currency');
+    }
+
+    /**
+     * Sets the request payment method ID.
+     *
+     * @param string $value
+     *
+     * @return $this
+     */
+    public function setPaymentMethodId($value)
+    {
+        return $this->setParameter('paymentMethodId', $value);
+    }
+
+    /**
+     * Get the request payment method ID.
+     * @return $this
+     */
+    public function getPaymentMethodId()
+    {
+        return $this->getParameter('paymentMethodId');
+    }
 
     /**
      * Prepare data to send
